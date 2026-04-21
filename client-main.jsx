8821e3167734e3190710098fd6946cac02547500
nirhilu-design <nirhilu@gmail.com>
2026-04-21 17:33:45 +0300

    Update client-main.jsx

--- client-main.jsx
+++ client-main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import ClientPortalPage from "./ClientPortalPage";
+import ClientPortalPage from "./ClientPortalPage.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
