f8a5875f8de1064ebe1cc3a00790964378ff93f5
nirhilu-design <nirhilu@gmail.com>
2026-04-28 16:12:01 +0300

    Update ReportPage.jsx

--- ReportPage.jsx
+++ ReportPage.jsx
@@ -1823,7 +1823,7 @@ function DonutBreakdownCard({
 
   const percentText = (value) => {
     const num = Number(value || 0);
-    if (num < 0.05) return "0%";
+    if (num < 0.5) return "0%";
     return `${Math.round(num)}%`;
   };
 
